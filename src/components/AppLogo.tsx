6b01d80e3656ca4d74b8d09bf939dfcfde978858
WeAreCode045 <maurice@code045.nl>
2026-04-04 02:44:43 +0200

    Refactor AppLogo component and simplify sign-in page layout
    - Updated the `AppLogo` component to remove the optional `logoDarkClassName` prop for a cleaner implementation.
    - Simplified the layout of the sign-in page by adjusting the `Card` component and streamlining the `auth-brand` section, enhancing overall readability and user experience.

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -4,12 +4,10 @@ import {Link} from "react-router";
 import logoDark from '@/assets/images/logo-black.png'
 import logo from '@/assets/images/logo.png'
 
-const AppLogo = ({ height, logoDarkClassName }: { height?: number; logoDarkClassName?: string }) => {
+const AppLogo = ({ height }: { height?: number }) => {
   return (
     <>
-      <Link
-        to="/"
-        className={['logo-dark', logoDarkClassName].filter(Boolean).join(' ')}>
+      <Link to="/" className="logo-dark">
         <img src={logoDark} alt="dark logo" height={height ?? 28} />
       </Link>
       <Link to="/" className="logo-light">
